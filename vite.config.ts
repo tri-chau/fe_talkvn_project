9d97dc57faf4b848f18d5d9ced11a6ea1375e4d6
tri-chau <trichau2511@gmail.com>
2025-04-08 23:34:48 +0700

    update: env and vite config for production

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import react from "@vitejs/plugin-react";
 import dotenv from "dotenv";
+import fs from "fs";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
@@ -11,6 +12,10 @@ export default defineConfig(({ mode }) => {
     plugins: [react()],
     server: {
       open: true,
+      https: {
+        key: fs.readFileSync("C:/Users/ASUS/Downloads/localhost-key.pem"), // Đường dẫn đến file key của bạn
+        cert: fs.readFileSync("C:/Users/ASUS/Downloads/localhost.pem"), // Đường dẫn đến file cert của bạn
+      },
     },
   };
 });
