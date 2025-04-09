8146cdbfb28c313f2bab655a16e3394c9169d50b
tri-chau <trichau2511@gmail.com>
2025-04-09 08:48:00 +0700

    fix: change to backend port

--- src/helpers/constants/configs.constant.ts
+++ src/helpers/constants/configs.constant.ts
@@ -1,3 +1,6 @@
-export const apiBaseUrl = "https://14.191.231.143:8085/api";
+// export const apiBaseUrl = "https://14.191.231.143:8085/api";
 
-export const socketBaseUrl = "https://14.191.231.143:8085/api";
+// export const socketBaseUrl = "https://14.191.231.143:8085/api";
+
+export const apiBaseUrl = "https://143.198.203.84:8085/api";
+export const socketBaseUrl = "https://143.198.203.84:8085/api";
