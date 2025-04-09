b8cfac8e10e900c91925b6f6b936bcfc9ebd7d67
tri-chau <trichau2511@gmail.com>
2025-04-09 13:48:07 +0700

    fix: update domain name with SSL

--- src/helpers/constants/configs.constant.ts
+++ src/helpers/constants/configs.constant.ts
@@ -2,5 +2,9 @@
 
 // export const socketBaseUrl = "https://14.191.231.143:8085/api";
 
-export const apiBaseUrl = "https://143.198.203.84:8085/api";
-export const socketBaseUrl = "https://143.198.203.84:8085/api";
+// export const apiBaseUrl = "https://143.198.203.84:8085/api";
+// export const socketBaseUrl = "https://143.198.203.84:8085/api";
+
+export const apiBaseUrl = "https://talkvn.id.vn/api";
+
+export const socketBaseUrl = "https://talkvn.id.vn/api";
