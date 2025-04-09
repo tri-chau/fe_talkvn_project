72dc86132459b3cb98a5ed279b92b9195caa8774
tri-chau <trichau2511@gmail.com>
2025-04-09 08:23:28 +0700

    fix: change port

--- src/helpers/constants/configs.constant.ts
+++ src/helpers/constants/configs.constant.ts
@@ -1,3 +1,3 @@
-export const apiBaseUrl = "https://14.191.231.143:8081/api";
+export const apiBaseUrl = "https://14.191.231.143:8085/api";
 
-export const socketBaseUrl = "https://14.191.231.143:8081/api";
+export const socketBaseUrl = "https://14.191.231.143:8085/api";
