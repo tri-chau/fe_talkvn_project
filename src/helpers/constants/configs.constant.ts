dbefd449536c915df9cb684399395bcbc068b8ed
Le Minh Hoang <le-minh-hoang@gitlab.minhhoang.org>
2025-04-10 22:20:35 +0700

    develop conversation features (create) + config VITE_API for local

--- src/helpers/constants/configs.constant.ts
+++ src/helpers/constants/configs.constant.ts
@@ -1,3 +1,7 @@
-export const apiBaseUrl = "https://18.138.250.74/api";
+// export const apiBaseUrl = "https://18.138.250.74/api";
 
-export const socketBaseUrl = "https://18.138.250.74";
+// export const socketBaseUrl = "https://18.138.250.74";
+
+export const apiBaseUrl = "http://localhost:8084/api";
+
+export const socketBaseUrl = "http://localhost:8084";
